05abdbdd7ec179924dc1418a48fce9b6d8093713
keith <keith.perkins@cnu.edu>
2020-04-21 14:18:39 -0400

    update gradle

--- app/src/main/java/com/example/perkins/br_dynamicregistered/MainActivity.java
+++ app/src/main/java/com/example/perkins/br_dynamicregistered/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.perkins.br_dynamicregistered;
 
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
